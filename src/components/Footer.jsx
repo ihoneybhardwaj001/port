8d4e4124e36a16464784eddc322eb8c527636fd7
Honey Bhardwaj <info.jaielectronics@gmail.com>
2025-10-24 12:25:03 +0530

    honey latest update on devops

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-[#915EFF] z-10 text-center text-xl flex items-center justify-center py-4">
-            &copy; Honey Bhardwaj 2024
+            &copy; Honey Bhardwaj 2025
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Honey</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm IT Admin And Linux Server Engineer,
+            I'm Devops And Linux Server Engineer,
             <br className="sm:block hidden" />
             Web3.0 Enthusiast.
           </p>
